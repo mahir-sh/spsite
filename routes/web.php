e1f9e977877001463c4766e7a985ad8881b00bfc
Mahir Shahriyar <mahir.shahriyar.polok@gmail.com>
2024-09-10 16:12:40 +0600

    fresh projects page

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/project', function () {
+    return view('project');
+});
 
+Route::get('/projects_details', function () {
+    return view('projects_details');
+});
